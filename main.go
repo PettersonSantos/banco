25d8e34aa55bea9a8eb426b4aa9f877464e4ef70
PettersonSantos <petterson.santos@outlook.com.br>
2021-07-19 13:01:54 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Petterson"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float32 = 530.6
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+}
