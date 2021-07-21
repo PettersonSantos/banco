23ddd1ec18d492f22ad506ed960aa00e2900fa59
PettersonSantos <petterson.santos@outlook.com.br>
2021-07-21 16:42:41 -0300

    criacao de funcs de sque e deposito

--- main.go
+++ main.go
@@ -9,13 +9,33 @@ type ContaCorrente struct {
 	saldo float32
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float32) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if !podeSacar {
+		return "Saldo Insuficiente"
+	}
+	c.saldo -= valorDoSaque
+	return "Saldo Realizado com Sucesso"
+}
+
+func (c *ContaCorrente) DepositarSaldo(valorDoDeposito float32) (string, float32) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito Realizado com Sucesso", c.saldo
+	}
+
+	return "O valor do depósito menor que 0", c.saldo
+}
+
 func main() {
 	corrente := ContaCorrente{
 		titular:       "Petterson",
 		numeroAgencia: 556,
 		numeroConta:   56578,
-		saldo:         535.2,
+		saldo:         500,
 	}
 
-	fmt.Println(corrente)
+	fmt.Println(corrente.saldo)
+    status, valor := corrente.DepositarSaldo(-100)
+	fmt.Println(status, "Novo saldo ", valor)
 }
